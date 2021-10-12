91dd33ba7edfffff2acdeae3fecefbb0a1d872bd
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-12 13:06:35 -0500

    created refresh handler to refreshing order information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import OrderList from './OrderList'
+import OrderList from './components/OrderList'
 import "./App.css";
 
 function App() {
---
+++ src/components/OrderList.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const OrderList = (props) => {
+    const [orders, setOrders] = useState(props.orders)
+    const [error, setError] = useState("")
+    //refresh orders
+    const refreshHandler = () => {
+        fetch("http://localhost:3000/routes/orders", {
+            method: "GET",
+            mode: "cors"
+        })
+        .then((result) => result.json())
+        .then((res) => {
+            setOrders(res.result)
+        })
+        .catch((err) => {
+            setError(err.message)
+        })
+    }
+
+}
+
+export default OrderList
