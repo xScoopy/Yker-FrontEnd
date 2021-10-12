0bb59504d6e6156430dd2edf1e8bbf2653dc4ca1
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-12 13:52:54 -0500

    added base code for modal

---
+++ src/components/Modal.js
@@ -0,0 +1,25 @@
+import "./Modal.css";
+
+const Modal = (props) => {
+    if(!props.show) {
+        return null;
+    }
+    return (
+        <div className="modal" onClick={props.onClose}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-header">
+              <h4 className="modal-title">Order Details</h4>
+            </div>
+            <div className="modal-body">
+            </div>
+            <div className="modal-footer">
+              <button onClick={props.onClose} className="button">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default Modal
